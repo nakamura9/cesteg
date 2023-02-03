6644d35e09a77f73a015f576baad16b26025ef21
Caleb Kandoro <kandoroc@hotmail.com>
2023-02-03 05:33:27 +0200

    intial commit

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import Products from './products'
-import News from './news'
-
-ReactDOM.render(<News />, document.getElementById('news-root'))
-ReactDOM.render(<Products />, document.getElementById('products-root'))
